07a8b302706232d53e16b9e5973d650a3fb6a954
gldeng <realgldeng@gmail.com>
2024-11-18 16:23:09 +0800

    Use aelf proto in aelf namespace

--- core/src/pb/mod.rs
+++ core/src/pb/mod.rs
@@ -1,14 +1,10 @@
 // @generated
+// @@protoc_insertion_point(attribute:aelf)
+pub mod aelf {
+    include!("aelf.rs");
+    // @@protoc_insertion_point(aelf)
+}
 pub mod sf {
-    pub mod aelf {
-        pub mod r#type {
-            // @@protoc_insertion_point(attribute:sf.aelf.type.v1)
-            pub mod v1 {
-                include!("sf.aelf.type.v1.rs");
-                // @@protoc_insertion_point(sf.aelf.type.v1)
-            }
-        }
-    }
     // @@protoc_insertion_point(attribute:sf.substreams)
     pub mod substreams {
         include!("sf.substreams.rs");
