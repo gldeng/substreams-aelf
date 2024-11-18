98d9d1d7868a8453c080931dc1af7679ce2c45d6
gldeng <realgldeng@gmail.com>
2024-11-18 18:00:59 +0800

    Make `address!` return Option

--- substreams-aelf/src/lib.rs
+++ substreams-aelf/src/lib.rs
@@ -25,7 +25,7 @@ fn b58_to_address(b58_string: &str) -> Result<Address, String> {
     }
 
     if data.len() != 32 {
-        return        Err("Invalid address".to_string());
+        return Err("Invalid address".to_string());
     }
 
     let address = Address {
@@ -38,7 +38,7 @@ fn b58_to_address(b58_string: &str) -> Result<Address, String> {
 #[macro_export]
 macro_rules! address {
     ($string:expr) => {{
-        b58_to_address($string).unwrap()
+        b58_to_address($string).ok()
     }};
 }
 
@@ -50,9 +50,9 @@ mod tests {
     #[test]
     fn test_address() {
         let address = address!("2DZER7qHVwv3PUMFsHuQaQbE4wDFsCRzJsxLwYEk8rgM3HVn1S");
-        let expected = Address{
+        let expected = Some(Address {
             value: vec![0xa0, 0x34, 0x93, 0x82, 0xb8, 0x9d, 0x5f, 0x20, 0x72, 0x76, 0xf0, 0x98, 0x3a, 0x4, 0x1e, 0x5a, 0x11, 0x31, 0x34, 0xb8, 0x97, 0x88, 0x8e, 0x39, 0x99, 0x1c, 0x5b, 0x59, 0xde, 0x23, 0xd, 0x16]
-        };
+        });
 
         assert_eq!(address, expected);
     }
