21980aca5f934741f199201d3518a0f39439da93
gldeng <realgldeng@gmail.com>
2024-11-21 16:35:50 +0800

    Add firehose version of aelf blocks

--- core/src/pb/mod.rs
+++ core/src/pb/mod.rs
@@ -5,6 +5,15 @@ pub mod aelf {
     // @@protoc_insertion_point(aelf)
 }
 pub mod sf {
+    pub mod aelf {
+        pub mod r#type {
+            // @@protoc_insertion_point(attribute:sf.aelf.type.v1)
+            pub mod v1 {
+                include!("sf.aelf.type.v1.rs");
+                // @@protoc_insertion_point(sf.aelf.type.v1)
+            }
+        }
+    }
     // @@protoc_insertion_point(attribute:sf.substreams)
     pub mod substreams {
         include!("sf.substreams.rs");
